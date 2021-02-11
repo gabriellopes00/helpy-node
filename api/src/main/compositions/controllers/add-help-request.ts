3aae5e091af455e00363d4c78ea748ba1dace728
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-11 15:49:24 -0300

    fix(api): add RequiredFieldsType validaiton to AddHelpRequest controller

--- api/src/main/compositions/controllers/add-help-request.ts
+++ api/src/main/compositions/controllers/add-help-request.ts
@@ -1,6 +1,7 @@
 import { DbAddHelpRequest } from '@src/implementation/usecases/help-requests/add-help-request'
 import { MongoHelpRequestRepository } from '@src/infra/database/help-requests/repository'
 import { AddHelpRequestController } from '@src/presentation/controllers/add-help-request'
+import { RequiredFieldsTypeValidation } from '@src/presentation/validation/usecases/required-fields-type-validation'
 import { RequiredFieldsValidation } from '@src/presentation/validation/usecases/required-fields-validation'
 import { ValidationCompositor } from '@src/presentation/validation/usecases/validation-compositor'
 
@@ -9,7 +10,8 @@ const helpRequestRepository = new MongoHelpRequestRepository()
 const dbAddHelpRequest = new DbAddHelpRequest(helpRequestRepository)
 
 const validations = new ValidationCompositor([
-  new RequiredFieldsValidation(['latitude', 'longitude'])
+  new RequiredFieldsValidation(['latitude', 'longitude']),
+  new RequiredFieldsTypeValidation()
 ])
 
 export const addHelpRequestController = new AddHelpRequestController(
