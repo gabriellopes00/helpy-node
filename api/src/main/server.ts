6ea5a01b9e930e8e1204dea150696dc95e5dc469
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-11 15:38:42 -0300

    feat:(api): add pino logger

--- api/src/main/server.ts
+++ api/src/main/server.ts
@@ -1,18 +1,15 @@
 import 'module-alias/register'
 import { close, connect } from '../infra/database/helpers/mongoose'
 import { port } from '../config/env'
-// import logger from '../config/logger'
+import logger from '../config/logger'
 ;(async () => {
   try {
     await connect()
-    // logger.info('Mongodb connected successfully')
-    console.log('db connected')
+    logger.info('Mongodb connected successfully')
 
     const app = (await import('./config/app')).default
     const server = app.listen(port, () => {
-      console.log('server started')
-
-      // logger.info(`Server running at http://localhost:${port}`)
+      logger.info(`Server running at http://localhost:${port}`)
     })
 
     const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT']
@@ -21,29 +18,29 @@ import { port } from '../config/env'
         try {
           await close()
           server.close()
-          // logger.info('Server stopped successfully')
+          logger.info('Server stopped successfully')
           process.exit(0)
         } catch (error) {
-          // logger.error(`App exited with error: ${error}`)
+          logger.error(`App exited with error: ${error}`)
           process.exit(1)
         }
       })
     }
   } catch (error) {
-    // logger.error(error)
+    logger.error(error)
     process.exit(1)
   }
 })()
 
 // Errors handles
 process.on('unhandledRejection', (reason, promise) => {
-  // logger.error(
-  //   `App exiting due an unhandled promise: ${promise} and reason: ${reason}`
-  // )
+  logger.error(
+    `App exiting due an unhandled promise: ${promise} and reason: ${reason}`
+  )
   throw reason
 })
 
 process.on('uncaughtException', error => {
-  // logger.error(`App exiting due to an uncaught exception: ${error}`)
+  logger.error(`App exiting due to an uncaught exception: ${error}`)
   process.exit(0)
 })
