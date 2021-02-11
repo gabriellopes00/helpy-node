2f3acb0ad169ff0a3bd4140586fb6e45651035e5
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-02-11 15:10:51 -0300

    refactor: change required fields validation to receive a list of fields

--- api/src/presentation/validation/usecases/__tests__/required-fields-validation.spec.ts
+++ api/src/presentation/validation/usecases/__tests__/required-fields-validation.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamError } from '@src/presentation/errors/missing-param-error'
 import { RequiredFieldsValidation } from '../required-fields-validation'
 
 describe('RequiredField Validation', () => {
-  const sut = new RequiredFieldsValidation('latitude')
+  const sut = new RequiredFieldsValidation(['latitude', 'longitude'])
   test('Should return a MissingParamError if validation fails', () => {
     const error = sut.validate({ longitude: -23.168516 })
     expect(error).toEqual(new MissingParamError('latitude'))
--- api/src/presentation/validation/usecases/required-fields-validation.ts
+++ api/src/presentation/validation/usecases/required-fields-validation.ts
@@ -2,9 +2,11 @@ import { MissingParamError } from '@src/presentation/errors/missing-param-error'
 import { Validation } from '../interfaces/validation'
 
 export class RequiredFieldsValidation implements Validation {
-  constructor(private readonly field: string) {}
+  constructor(private readonly fields: string[]) {}
 
   validate(data: any): Error {
-    if (!data[this.field]) return new MissingParamError(this.field)
+    for (const field of this.fields) {
+      if (!data[field]) return new MissingParamError(field)
+    }
   }
 }
