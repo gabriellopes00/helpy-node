bfc3e340a704e3cd7cfa6b1bf0ec9a717d2b2437
henrique2m <btdavintehenrique@gmail.com>
2021-03-15 18:38:36 -0300

    chore: home page implementation

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useCallback } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
+import { Image } from 'react-native';
+import {
+  Container,
+  LogoBackground,
+  Header,
+  HeaderTitle,
+  Footer,
+  ButtonFooter,
+  ButtonTextFooter,
+} from './styles';
 
-import { Container } from './styles';
+import logoBackgroundImage from '../../assets/backgroundLogo/logo.png';
+
+import iconVolumeUp from '../../assets/volumeUp/volume_up.png';
 
 const Home: React.FC = () => {
-  const navigation = useNavigation();
+  const { navigate } = useNavigation();
+
+  const navigateToPageSOS = useCallback(() => {
+    navigate('SOS');
+  }, [navigate]);
 
   return (
     <Container>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('SOS');
-        }}
+      <Header>
+        <HeaderTitle>Help Women</HeaderTitle>
+      </Header>
+
+      <LogoBackground
+        source={logoBackgroundImage}
+        imageStyle={{ resizeMode: 'stretch' }}
       >
-        <Text style={{ color: '#000' }}>Home</Text>
-      </TouchableOpacity>
+        <Footer>
+          <Image source={iconVolumeUp} />
+
+          <ButtonFooter onPress={navigateToPageSOS}>
+            <ButtonTextFooter> PEDIR SOCORRO </ButtonTextFooter>
+          </ButtonFooter>
+        </Footer>
+      </LogoBackground>
     </Container>
   );
 };
