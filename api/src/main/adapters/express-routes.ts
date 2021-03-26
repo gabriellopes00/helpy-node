8d206e37e87865997b041a3626f90e836f238a29
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-03-26 09:24:01 -0300

    refactor(api): refactor main layer code style

--- api/src/main/adapters/express-routes.ts
+++ api/src/main/adapters/express-routes.ts
@@ -4,14 +4,10 @@ import { HttpRequest } from '@/presentation/interfaces/http'
 
 export const adaptRoutes = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    const httpRequest: HttpRequest = {
-      body: req.body
-    }
+    const httpRequest: HttpRequest = { body: req.body }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
-    } else {
-      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
-    }
+    } else res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
   }
 }
