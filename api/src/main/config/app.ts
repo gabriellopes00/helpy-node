0fa02f50d74c30be1a79594e30c87bff6f58285f
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-03-26 09:00:43 -0300

    refactor(api): refactor server web socket integration

--- api/src/main/config/app.ts
+++ api/src/main/config/app.ts
@@ -1,24 +1,14 @@
 import express from 'express'
-import Middlewares from './middlewares'
-import Routes from '../routes'
 import { createServer } from 'http'
-import { Server } from 'socket.io'
-
-import { WebSocketManager } from '@/presentation/web-socket/web-socket'
-import { SocketIo } from '@/infra/web-socket/socketio'
-import { eventEmitter } from '../compositions/event/event-emitter'
+import Routes from '../routes'
+import Middlewares from './middlewares'
+import WebSocket from './web-socket'
 
 const app = express()
 Middlewares(app)
 Routes(app)
 
 const httpServer = createServer(app)
-const io = new Server(httpServer, { cors: { origin: '*' } })
-io.on('connection', socket => {
-  const socketIo = new SocketIo(socket)
-  const webSocketManager = new WebSocketManager(eventEmitter, socketIo)
-  webSocketManager.handle()
-  console.log('Connection created successfully')
-})
+WebSocket(httpServer)
 
 export default httpServer
---
+++ api/src/main/config/web-socket.ts
@@ -0,0 +1,14 @@
+import { SocketIo } from '@/infra/web-socket/socketio'
+import { WebSocketManager } from '@/presentation/web-socket/web-socket'
+import { Server } from 'http'
+import { Server as IoServer } from 'socket.io'
+import { eventEmitter } from '../compositions/event/event-emitter'
+
+export default (server: Server): void => {
+  const io = new IoServer(server, { cors: { origin: '*' } })
+  io.on('connection', socket => {
+    const socketIo = new SocketIo(socket)
+    const webSocketManager = new WebSocketManager(eventEmitter, socketIo)
+    webSocketManager.handle()
+  })
+}
