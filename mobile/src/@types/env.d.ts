6e417af1f03de3d85a1167010d6c7fe6cec606e7
henrique2m <btdavintehenrique@gmail.com>
2021-04-12 17:36:27 -0300

    feat: :sparkles: add user distress call :rocket:

---
+++ mobile/src/@types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const APP_MAPBOX_KEY: string;
+  export const APP_API_URL: string;
+}
--- mobile/src/pages/SOS/styles.ts
+++ mobile/src/pages/SOS/styles.ts
@@ -1,7 +1,52 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
+`;
+
+export const ContainerButtonSOS = styled.View`
+  width: 115px;
+  height: 115px;
+  background-color: rgba(255, 122, 0, 0.2);
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 100px;
+  z-index: 2;
+  border-radius: 57.5px;
+`;
+
+export const ButtonSOS = styled(RectButton)`
+  width: 100px;
+  height: 100px;
+  background: #ff7a00;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 50px;
+`;
+
+export const ButtonSOSTitle = styled.Text`
+  font-family: 'Roboto-Bold';
+  font-size: 36px;
+  color: #fff;
+`;
+
+export const UserPositionMarkup = styled.View`
+  width: 30px;
+  height: 30px;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(246, 60, 60, 0.2);
+  border-radius: 15px;
+`;
+
+export const UserPositionMarkupCenter = styled.View`
+  width: 15px;
+  height: 15px;
+  background: #f63c3c;
+  border-radius: 7.5px;
 `;
